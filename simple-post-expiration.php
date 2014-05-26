94d5bbcc93d82c259f95e5bd8907dc973287ca8f
Pippin Williamson <pippin@pippinsplugins.com>
2014-05-26 12:33:54 -0500

    Extended plugin header

--- simple-post-expiration.php
+++ simple-post-expiration.php
@@ -7,6 +7,21 @@
  * Author: Pippin Williamson
  * Author URI: http://pippinsplugins.com
  * Contributors: mordauk, rzen
+ * Text Domain: pw-spe
+ * Domain Path: languages
+ *
+ * Simple Post Expiration is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Simple Post Expiration is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Simple Post Expiration. If not, see <http://www.gnu.org/licenses/>.
 */
 
 define( 'PW_SPE_ASSETS_URL', plugin_dir_url( __FILE__ ) . 'assets' ) ;
